9a54a98efd0e20e7762ef786fff45c6588cc2b15
Justin Svetlik <jsvetlik@smiaware.com>
2025-04-28 15:43:25 -0500

    initial commit

---
+++ app/controllers/uploads_controller.ts
@@ -0,0 +1,35 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import app from '@adonisjs/core/services/app'
+import logger from '@adonisjs/core/services/logger'
+import { createWriteStream } from 'fs'
+import { pipeline } from 'stream/promises'
+
+export default class UploadsController {
+  async store({ request }: HttpContext) {
+    /**
+     * Step 1: Define a file listener
+     */
+    request.multipart.onFile('*', {}, async (part, reporter) => {
+      part.pause()
+      part.on('data', reporter)
+
+      part.on('error', (error) => {
+        logger.error(error)
+      })
+
+      const filePath = app.makePath(part.file.clientName)
+      await pipeline(part, createWriteStream(filePath))
+      return { filePath }
+    })
+    
+    /**
+     * Step 2: Process the stream
+     */
+    await request.multipart.process()
+
+    /**
+     * Step 3: Access processed files
+     */
+    return request.allFiles()
+  }
+}
